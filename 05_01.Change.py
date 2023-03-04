63de1297b55f74dd711fb3a9b8709d955dbcebfc
arshsinghphd <arshsinghphd@gmail.com>
2023-03-03 21:42:45 -0800

    Update 05_01.Change.py

--- 05_01.Change.py
+++ 05_01.Change.py
@@ -1,28 +1,14 @@
-# Uses python3
-import sys
-
-def get_change(m):
-    '''
-    change money using 1, 3, 4
-    '''
-    coins = [1, 3, 4]
-    dict_m = {}
+def change(money):
+    # write your code here
+    table = [float('inf') for _ in range(money + 1)]
+    table[0] = 0
     
-    for c in coins: # fill initial values
-        dict_m[c] = 1
-        
-    for amt in range(min(coins), m):
-        for c in coins:
-            if amt + c in dict_m:
-                if dict_m.get(amt + c) > dict_m.get(amt) + 1:
-                    dict_m[amt + c] = dict_m.get(amt) + 1
-            else:
-                dict_m[amt + c] = dict_m.get(amt) + 1
-
-    #print(sorted([i for i in zip(dict_m.keys(), dict_m.values())], key=lambda x: x[0]))
-        
-    return dict_m[m]
+    for m in range(1, money + 1):
+        for c in [1,3,4]:
+            if c <= m:
+                table[m] = min(table[m], 1 + table[m - c])
+    return table[money]
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
-    print(get_change(m))
+    m = int(input())
+    print(change(m))
