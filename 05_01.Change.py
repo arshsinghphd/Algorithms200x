33cc241bdb83b3fb62d73d4549f2783c916c7e12
arshsinghphd <arshsinghphd@gmail.com>
2023-03-04 19:15:35 -0800

    Update 05_01.Change.py

--- 05_01.Change.py
+++ 05_01.Change.py
@@ -4,11 +4,11 @@ def change(money):
     table[0] = 0
     
     for m in range(1, money + 1):
-        for c in [1,3,4]:
+        for c in [1, 5, 6]:
             if c <= m:
                 table[m] = min(table[m], 1 + table[m - c])
     return table[money]
 
 if __name__ == '__main__':
-    m = int(input())
-    print(change(m))
+    #m = int(input())
+    print(change(28))
